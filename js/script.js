55992bf44e5b1e964a0ba57d46e425ee26a69adc
Ediz <edizyurdakul@gmail.com>
2020-05-25 14:08:22 +0300

    Progress bar and margin fixes. Progress bar is clickable as well

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ const progressBar = document.getElementById('progress-bar');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
-const music = ['Herbal Tea','Heroes Tonight']
- 
+const music = ['Herbal Tea', 'Heroes Tonight']
+
 let musicIndex = 1;
 
 loadMusic(music[musicIndex]);
@@ -24,19 +24,19 @@ function playMusic() {
     musicContainer.classList.add('play');
     btnPlay.querySelector('i.fa').classList.remove('fa-play')
     btnPlay.querySelector('i.fa').classList.add('fa-pause')
-    audio.play(); 
+    audio.play();
 }
 
 function pauseMusic() {
     musicContainer.classList.remove('play');
     btnPlay.querySelector('i.fa').classList.add('fa-play')
     btnPlay.querySelector('i.fa').classList.remove('fa-pause')
-    audio.pause(); 
+    audio.pause();
 }
 
 function prevMusic() {
     musicIndex--;
-    if(musicIndex < 0){
+    if (musicIndex < 0) {
         musicIndex = music.length - 1;
     }
 
@@ -45,23 +45,38 @@ function prevMusic() {
 
 function nextMusic() {
     musicIndex++;
-    if(musicIndex > music.length - 1){
+    if (musicIndex > music.length - 1) {
         musicIndex = 0;
     }
 
     loadMusic(music[musicIndex]);
 }
 
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercentage = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercentage}%`;
+}
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
 
 
 btnPlay.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
     if (isPlaying) {
         pauseMusic()
-    }else {
+    } else {
         playMusic()
     }
 })
 
 btnPrev.addEventListener('click', prevMusic);
-btnNext.addEventListener('click', nextMusic)
+btnNext.addEventListener('click', nextMusic);
+audio.addEventListener('timeupdate', updateProgress);
+progressBar.addEventListener('click', setProgress);
+audio.addEventListener('ended', nextMusic);
